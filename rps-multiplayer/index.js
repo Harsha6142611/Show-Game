2864c5f8f8453a0e2aad0eed93945068f8422be7
Harsha6142611 <pasupulaharsha7006@gmail.com>
2024-10-12 12:02:52 +0530

    removed local frontend url

--- rps-multiplayer/index.js
+++ rps-multiplayer/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173','https://show-game-1.onrender.com/'], 
+    origin: '*', 
     methods: ['GET', 'POST'],
   },
 });
